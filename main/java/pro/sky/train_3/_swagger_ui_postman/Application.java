2a4f8dcb35482cfadca773e25840dccb5e6d283c
Solpostgit <begemot_off@mail.ru>
2024-01-22 20:58:59 +0300

    Initial commit

---
+++ main/java/pro/sky/train_3/_swagger_ui_postman/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.train_3._swagger_ui_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ test/java/pro/sky/train_3/_swagger_ui_postman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.train_3._swagger_ui_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
